246cb319a2046fbd784001d9ad6f170463d6ea51
Abdujabborov Kamronbek <komronabdujabbor@gmail.com>
2023-12-18 20:54:35 +0500

    working on multiple item carousel

---
+++ script.js
@@ -0,0 +1,16 @@
+let items = document.querySelectorAll('.multiple')
+
+items.forEach((el) => {
+    const minPerSlide = 3
+    let next = el.nextElementSibling
+    for (var i=1; i<minPerSlide; i++) {
+        console.log(items[i])
+        if (!next) {
+            // wrap carousel by using first child
+        	next = items[0]
+      	}
+        let cloneChild = next.cloneNode(true)
+        el.appendChild(cloneChild.children[0])
+        next = next.nextElementSibling
+    }
+})
