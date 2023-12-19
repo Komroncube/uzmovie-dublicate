c4927d001965f9cd45c738efac9f939a370ed44b
Abdujabborov Kamronbek <komronabdujabbor@gmail.com>
2023-12-19 18:22:37 +0500

    for carousel

--- script.js
+++ script.js
@@ -1,16 +1,61 @@
-let items = document.querySelectorAll('.multiple')
+function initializeCarousel(carouselSelector) {
+    const multipleItemCarousel = document.querySelector(carouselSelector);
 
-items.forEach((el) => {
-    const minPerSlide = 3
-    let next = el.nextElementSibling
-    for (var i=1; i<minPerSlide; i++) {
-        console.log(items[i])
-        if (!next) {
-            // wrap carousel by using first child
-        	next = items[0]
-      	}
-        let cloneChild = next.cloneNode(true)
-        el.appendChild(cloneChild.children[0])
-        next = next.nextElementSibling
+    if (window.matchMedia("(min-width:576px)").matches) {
+        // Check if Bootstrap is defined and loaded
+        if (typeof bootstrap !== 'undefined') {
+            window.addEventListener('load', function () {
+                const carousel = new bootstrap.Carousel(multipleItemCarousel, {
+                    interval: 5000
+                });
+
+                var carouselWidth = $(carouselSelector + ' .carousel-inner')[0].scrollWidth;
+                var cardWidth = $(carouselSelector + ' .carousel-item').width();
+                var scrollPosition = 0;
+
+                $(carouselSelector + ' .carousel-control-next').on('click', function () {
+                    if (scrollPosition < (carouselWidth - (cardWidth * 3))) {
+                        console.log('next');
+                        scrollPosition = scrollPosition + cardWidth;
+                        $(carouselSelector + ' .carousel-inner').animate({
+                            scrollLeft: scrollPosition
+                        }, 600);
+                    } else {
+                        // Return to the first item
+                        scrollPosition = 0;
+                        $(carouselSelector + ' .carousel-inner').animate({
+                            scrollLeft: scrollPosition
+                        }, 600);
+                    }
+                });
+
+                $(carouselSelector + ' .carousel-control-prev').on('click', function () {
+                    if (scrollPosition > 0) {
+                        console.log('prev');
+                        scrollPosition = scrollPosition - cardWidth;
+                        $(carouselSelector + ' .carousel-inner').animate({
+                            scrollLeft: scrollPosition
+                        }, 600);
+                    } else {
+                        // Go to the last item
+                        scrollPosition = carouselWidth - (cardWidth * 3);
+                        $(carouselSelector + ' .carousel-inner').animate({
+                            scrollLeft: scrollPosition
+                        }, 600);
+                    }
+                });
+            });
+        } else {
+            console.error("Bootstrap is not defined. Make sure you have included Bootstrap JavaScript.");
+        }
+    } else {
+        $(multipleItemCarousel).addClass('slide');
+        console.log("xato");
     }
-})
+}
+
+// Call the function for each carousel you want to initialize
+initializeCarousel("#multipleFilmsPart1");
+initializeCarousel("#multipleFilmsPart2");
+initializeCarousel("#multipleFilmsPart3");
+// Add more calls if needed
